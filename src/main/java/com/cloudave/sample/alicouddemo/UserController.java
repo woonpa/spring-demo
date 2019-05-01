480ca0816d68b3887f99c3732a32abd05e33ca83
朴 云波 <woonpa@example.com>
2019-05-01 19:21:40 +0900

    Modify

--- src/main/java/com/cloudave/sample/alicouddemo/UserController.java
+++ src/main/java/com/cloudave/sample/alicouddemo/UserController.java
@@ -7,31 +7,32 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/v1")
+@RequestMapping(value = "/api/v1/users")
 public class UserController {
 
     private final UserMapper userMapper;
 
-    @GetMapping(value = "/getOne/{id}")
+    @GetMapping(value = "/{id}")
     public UserEntity getOne(@PathVariable final String id) {
         return this.userMapper.getOne(id);
     }
 
-    @PostMapping(value = "/updateOne", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PutMapping(value = "/{id}", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public void updateOne(@RequestBody final UserDTO userDTO) {
+    public void updateOne(@PathVariable final String id, @RequestBody final UserDTO userDTO) {
         UserEntity userEntity = userDTO.convertToUserEntity();
+        userEntity.setId(id);
         this.userMapper.updateOne(userEntity);
     }
 
-    @PutMapping(value = "/createOne", consumes= MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @PostMapping(consumes= MediaType.APPLICATION_JSON_UTF8_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void createOne(@RequestBody final UserDTO userDTO) {
         UserEntity userEntity = userDTO.convertToUserEntity();
         this.userMapper.createOne(userEntity);
     }
 
-    @DeleteMapping(value = "/deleteOne/{id}")
+    @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteOne(@PathVariable final String id) {
         this.userMapper.deleteOne(id);
